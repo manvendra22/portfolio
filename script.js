eb4b1d8997e6ba20bd7029965c0ad0a0835005cc
Manvendra Singh <mjdeora@gmail.com>
2020-07-28 22:48:16 +0530

    [Update] Animations

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 ScrollReveal(
-    { duration: 2500 }
+    { duration: 3000, viewFactor: 0.5 }
 ).reveal('.intro');
 
 ScrollReveal(
-    { delay: 1500, duration: 1500 }
+    { duration: 2000, delay: 1000 }
 ).reveal('.pulseBtn');
 
-ScrollReveal({ delay: 0, duration: 2500 }).reveal('.about-first');
-ScrollReveal({ delay: 0, duration: 2500 }).reveal('.about-second');
-ScrollReveal({ delay: 0, duration: 2500 }).reveal('.project');
-
+ScrollReveal({ delay: 0, duration: 3000, origin: 'bottom', distance: '60px' }).reveal('.about-first');
+ScrollReveal({ delay: 1500, duration: 3000 }).reveal('.about-second');
+ScrollReveal({ delay: 0, duration: 3000, interval: 3000, origin: 'left' }).reveal('.project_intro');
+ScrollReveal({ delay: 1500, duration: 3000, interval: 3000, origin: 'right' }).reveal('.project_image');
 
 const homeTarget = document.querySelector('#home');
 const aboutTarget = document.querySelector('#about');

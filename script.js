50548d9cd94de50dda9f694a06521ef66021923a
Manvendra Singh <mjdeora@gmail.com>
2020-08-28 21:56:31 +0530

    [Fix] Mobile 100vh bug

--- script.js
+++ script.js
@@ -150,3 +150,10 @@ function changeTheme() {
         document.documentElement.style.setProperty(key, value)
     });
 }
+
+// We listen to the resize event
+window.addEventListener('resize', () => {
+    // We execute the same script as before
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
